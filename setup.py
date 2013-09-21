19f6cbfe59053a88fc73cdd53bb3e6c5a8a70b55
Ian Good <ian.good@rackspace.com>
2013-09-21 12:14:05 -0400

    upped version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-slimta-redisstorage',
-      version='0.1.0',
+      version='0.1.1',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Adds a redis-based queue extension to python-slimta.',
